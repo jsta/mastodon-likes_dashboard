3c1c446256696302021687611ed0d838dfa40b5e
jsta <stachel2@msu.edu>
2022-12-21 16:21:50 -0700

    not using envvar anymore

--- scripts/get_toots.R
+++ scripts/get_toots.R
@@ -5,7 +5,7 @@ library(rtoot)
 
 if (!is.null(grep("comp", Sys.info()["nodename"]))) { # not on jsta local system
   # auth_setup(clipboard = TRUE)
-  verify_envvar()
+  # verify_envvar()
 } else {
   # auth_setup_default()
 }
